e6b11e10ead8137e4f7e818468d2f80269b84cf8
Rafael Jorge <rafaelcamillojorge@gmail.com>
2022-11-17 21:13:39 -0300

    buscar com vetor

---
+++ ex4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+int busca(int n, int nmax, int nmin){
+    int item;
+    
+    scanf("%d", &item);
+    int v[5] = {1,2,3,4,5};
+    if (n <= nmax)
+    {
+        int meio = (nmax + nmin) / 2;
+        if (v[meio] == item)
+        {
+           return meio; 
+        }
+        if(item > v[meio]){
+            nmin = meio + 1;
+        }if(item < v[meio]){
+            nmax = meio - 1;
+            meio = (nmax + nmin) / 2;
+        }
+        if(nmin > nmax){
+            return -1;
+        }
+        
+        busca(n + 1, nmax, nmin);
+    }
+}
+
+
+
+int main(){
+    printf("Seu numero foi encontrado no indice v[%d]", busca(0, 5, 0));
+}
